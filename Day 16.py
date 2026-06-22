30045c7cd55d373a183356e26f26be7db91f24c9
Salohiddin <symplecticfoundryvibe@gmail.com>
2026-06-22 22:25:40 +0900

    Add files via upload

---
+++ Day 16.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy=Turtle()
+print(timmy)
+
+my_screen=Screen()
+print(my_screen.canvheight)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+my_screen.exitonclick()
+
+
