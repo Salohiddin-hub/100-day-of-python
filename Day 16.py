6852cb6a75874bc3a219b18aed4ac06fb0430bcb
Salohiddin <symplecticfoundryvibe@gmail.com>
2026-07-13 02:46:31 +0900

    Refactor turtle code and add PrettyTable example

--- Day 16.py
+++ Day 16.py
@@ -1,13 +1,20 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+# timmy=Turtle()
+# print(timmy)
+# my_screen=Screen()
+# print(my_screen.canvheight)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+# my_screen.exitonclick()
 
-timmy=Turtle()
-print(timmy)
-
-my_screen=Screen()
-print(my_screen.canvheight)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
-my_screen.exitonclick()
-
-
+from prettytable import PrettyTable
+table = PrettyTable()
+# table.field_names=["City names", "Area", "Population"]
+# table.add_row(["Andijan", "1234", "1_411_500"])
+# table.add_row(["Fergana", "1123", "1_100_323"])
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align="c"
+table.valign="b"
+print(table)
